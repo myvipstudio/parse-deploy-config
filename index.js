a487eb6140958786ba196d6aec1ef12e95827534
Phil Powers <ppowers@gmail.com>
2025-06-24 02:44:20 -0400

    Package 'index.js' and its dependencies using ncc

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// This is packaged into a single file using the 'ncc' tool; this is required to include our code along with all of the
+// dependencies (eg: 'actions/core') in a single file which can be invoked from GitHub Actions
+// To package this file, run the following commands:
+//
+//      npm install -g @vercel/ncc # only required once on your machine
+//      npm install # install the dependencies for this project
+//      ncc build index.js -m --no-source-map-register
+//
+// After this is done, the output will be in the 'dist' directory which is where the GitHub Action will look for the
+// code to run.
+
 const core = require('@actions/core');
 const mergeConfig = require('./merge-config');
 
